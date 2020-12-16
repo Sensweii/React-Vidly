4f4d9810216da2d796cbf23f36da79dd9d516de7
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-16 21:14:00 +0800

    Add movie form.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
 import { getGenres } from '../services/fakeGenreService';
@@ -82,6 +83,13 @@ class Movies extends Component {
             selectedItem={this.state.selectedGenre} />
         </div>
         <div className='col'>
+          <Link
+            to='/movies/add'
+            className='btn btn-primary'
+            style={{ marginBottom: 20 }}
+          >
+            Add Movie
+          </Link>
           <p>Showing {totalCount} movies.</p>
           <MoviesTable
             movies={movies}
