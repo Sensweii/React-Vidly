5a442e1abda5b882078fa08813de1992894e8b1c
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-17 19:11:54 +0800

    Refactor for consistency.

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,6 +1,8 @@
 import { apiURL } from '../config.json';
 import http from './httpService';
 
+const genresAPI = apiURL + 'genres/'
+
 export function getGenres() {
-    return http.get(apiURL + 'genres/');
+    return http.get(genresAPI);
 }
