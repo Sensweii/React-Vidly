3ba294072322e9be7dc4a93908d179ba1e9471f2
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-17 22:07:41 +0800

    Add authentication functions that use jwt tokens stored in local.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,39 @@
+import jwtDecode from 'jwt-decode';
+
 import { apiURL } from '../config.json';
 import http from './httpService';
 
 const authAPI = apiURL + 'auth/'
+const tokenKey = 'token';
 
-export function login(email, password) {
-    return http.post(authAPI, {
-        email: email,
-        password: password
-    })
+export async function login(email, password) {
+  const { data: jwt } = await http.post(authAPI, {
+    email: email,
+    password: password
+  })
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function loginWithJWT(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export async function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  loginWithJWT,
+  logout,
+  getCurrentUser
 }
