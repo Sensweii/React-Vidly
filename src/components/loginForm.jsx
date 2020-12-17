6e131d16bacf0580a4b824f6a87f61885554aec4
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-17 21:16:58 +0800

    Update login form to include new authentication service.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Joi from 'joi-browser';
+import { login } from '../services/authService';
 
 import Form from './common/form';
 
@@ -14,8 +15,19 @@ class LoginForm extends Form {
     password: Joi.string().required().label('Password')
   }
 
-  doSubmit = () => {
-    console.log('Submitted')
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem('token', jwt);
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   }
 
   render() {
