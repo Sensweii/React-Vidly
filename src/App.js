71a3fc12b062a8168bb4a56585546b76255d0611
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-17 16:39:37 +0800

    Add toast container.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Customers from './components/customers';
 import Movies from './components/movies';
@@ -11,12 +12,14 @@ import Rentals from './components/rentals';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <NavBar />
         <main className='container'>
           <Switch>
